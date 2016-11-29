c51b44561ae24444998e616c398376fa6f5f5918
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2016-11-29 15:57:25 -0700

    add Contact component, route, view

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,12 +1,14 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
 import {AboutComponent} from "./components/about-component";
+import {ContactComponent} from "./components/contact-component";
 
-export const allAppComponents = [HomeComponent, AboutComponent];
+export const allAppComponents = [HomeComponent, AboutComponent, ContactComponent];
 
 export const routes: Routes = [
 	{path: "", component: HomeComponent},
-	{path: "about", component: AboutComponent}
+	{path: "about", component: AboutComponent},
+	{path: "contact", component: ContactComponent}
 ];
 
 export const appRoutingProviders: any[] = [];
---
+++ public_html/templates/contact.php
@@ -0,0 +1,7 @@
+<section class="welcome">
+	<div class="container">
+		<div class="jumbotron text-center">
+			<h1>This is the Contact View</h1>
+		</div>
+	</div>
+</section>
