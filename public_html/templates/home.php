020a87f19d608e1d21c5a19afef97bdab58619e2
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2016-11-29 15:22:30 -0700

    add /app files, /webpack files, /public_html files

---
+++ public_html/templates/home.php
@@ -0,0 +1,8 @@
+<section class="welcome">
+	<div class="container">
+		<div class="jumbotron text-center">
+			<h1>Welcome to the Angular Template Demo #2!</h1>
+			<p class="lead">This is a more advanced implementation of Angular2 Route :D</p>
+		</div>
+	</div>
+</section>
---
+++ webpack/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/"; ?>" />
+
+		<title>Angular 2 Template Demo 2</title>
+	</head>
+	<body>
+		<!-- This custom tag much match your Angular @Component selector name in app/app.component.ts -->
+		<template2-app>Loading&hellip;</template2-app>
+	</body>
+</html>
