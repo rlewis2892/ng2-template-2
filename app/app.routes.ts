4bf3d8470a0440eee3256546abbc4c735d8b3235
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2016-11-29 17:09:19 -0700

    added 404 page not found view, route, component

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,16 +1,18 @@
 import {RouterModule, Routes} from "@angular/router";
+import {PageNotFoundComponent} from "./components/pagenotfound-component";
 import {HomeComponent} from "./components/home-component";
 import {AboutComponent} from "./components/about-component";
 import {ContactComponent} from "./components/contact-component";
 
-export const allAppComponents = [HomeComponent, AboutComponent, ContactComponent];
+export const allAppComponents = [PageNotFoundComponent, HomeComponent, AboutComponent, ContactComponent];
 
 // Place more specific routes above less specific routes - "first match wins" strategy.
 export const routes: Routes = [
 	{path: "about", component: AboutComponent},
 	{path: "contact", component: ContactComponent},
-	{path: "", component: HomeComponent}
+	{path: "", component: HomeComponent},
 	// wildcard route for 404 not found
+	{path: "**", component: PageNotFoundComponent}
 ];
 
 export const appRoutingProviders: any[] = [];
---
+++ public_html/templates/pagenotfound.php
@@ -0,0 +1,7 @@
+<section class="welcome">
+	<div class="container">
+		<div class="jumbotron text-center">
+			<h1>404 Page Not Found!</h1>
+		</div>
+	</div>
+</section>
