7fed17f07824321e137002bbdcb7bc5988a5217a
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2016-12-14 10:55:44 -0700

    added 404 page not found view, route, component

--- app/components/pagenotfound-component.ts
+++ app/components/pagenotfound-component.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 import {Router} from "@angular/router";
 
 @Component({
-	templateUrl: "./templates/pagenotfound.php"
+	templateUrl: "./templates/pagenotfound.php",
 })
 
 export class PageNotFoundComponent { }
